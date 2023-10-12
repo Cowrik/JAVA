ccc5b67dd7639a17cd0a9f2a9d0fcd298ffac12a
Sardor <Sardor>
2023-10-12 16:53:14 +0500

    JAVA

---
+++ java.js
@@ -0,0 +1,17 @@
+var name = prompt("Ismingiz nima")
+console.log("Mening ismim " + name)
+var age = prompt("Yoshingiz nechida")
+console.log("Mening yoshim " + age + " da")
+var email = prompt("Elekton pochtangiz")
+console.log("Mening eletron pochtam " + email)
+var misol = prompt(" Misolni yeching 6+3=?")
+console.log("Misol 1: " + '6+3=' + (6 + 3) + (" Sizning javobingiz ") + misol);
+var misol = prompt("Misolni yeching 35*89")
+console.log("Misol 2: " + '35*89=' + (35 * 89) + (" Sizning javobingiz ") + misol);
+var misol = prompt("Misolni yeching 12/8")
+console.log("Misol 3: " + '12/8=' + (12 / 1.12) + (" Sizning javobingiz ") + misol);
+var misol = prompt("Misolni yeching 100+450")
+console.log("Misol 4: " + '100+1.12=' + (100 * 1.12) + (" Sizning javobingiz ") + misol);
+var misol = prompt("Misolni yeching 12-123")
+console.log("Misol 5: " + '12-123=' + (12 - 123) + (" Sizning javobingiz ") + misol);
+alert("Natijangizni ko'rish uchun konsolni oching.");
